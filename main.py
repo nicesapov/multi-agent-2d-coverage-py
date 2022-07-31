ebfae4a043b061d9db9bb19df30a54e505d94f4d
Viacheslav Sokolov <soko.slav@yandex.ru>
2022-07-31 12:18:03 +0300

    Add F-plot

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ def main():
 #    ax = fig.add_subplot(111)
 
     fig, ax = plt.subplots()
+#    gr = fig.add_axes()
 #    fig.set_figwidth(10)#X_MAX - X_MIN)
 #    fig.set_figheight(12)#Y_MAX - Y_MIN)
     agents = [Agent(x, y, c, a) for [x, y, c, a] in INITIAL_POSITIONS]
 
+    xs = []
+    ys = []
+
     def animate(t):
+        nonlocal xs
+        nonlocal ys
+        nonlocal fig
         nonlocal agents
         print(t)
         ax.clear()
@@ -33,7 +40,10 @@ def main():
         agents = updated
         [plt.plot(agent.p[0], agent.p[1], marker='o', color=agent.color) for agent in agents]
 
-        return cs
+        xs += [t]
+        ys += [[agent.prev_f for agent in agents]] 
+
+        return fig 
 
     anime = animation.FuncAnimation(
         fig,
@@ -49,6 +59,13 @@ def main():
         fps=15,
         dpi=120,
     )
+
+    del anime
+
+    fig.clear()
+    plt.plot(xs, ys)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
